1391e3e5f1fc1827a1797436733e48bc4f24d549
Sourcery AI <>
2022-04-12 05:07:03 +0000

    'Refactored by Sourcery'

--- tag_generator.py
+++ tag_generator.py
@@ -10,45 +10,44 @@ This script creates tags for your Jekyll blog hosted by Github page.
 No plugins required.
 '''
 
+
 import glob
 import os
 
 post_dir = '_posts/'
 tag_dir = 'tag/'
 
-filenames = glob.glob(post_dir + '*md')
+filenames = glob.glob(f'{post_dir}*md')
 
 total_tags = []
 for filename in filenames:
-    f = open(filename, 'r', encoding='utf8')
-    crawl = False
-    for line in f:
-        if crawl:
-            current_tags = line.strip().split()
-            if current_tags[0] == 'tags:':
-                total_tags.extend(current_tags[1:])
-                crawl = False
-                break
-        if line.strip() == '---':
-            if not crawl:
-                crawl = True
-            else:
-                crawl = False
-                break
-    f.close()
+    with open(filename, 'r', encoding='utf8') as f:
+        crawl = False
+        for line in f:
+            if crawl:
+                current_tags = line.strip().split()
+                if current_tags[0] == 'tags:':
+                    total_tags.extend(current_tags[1:])
+                    crawl = False
+                    break
+            if line.strip() == '---':
+                if crawl:
+                    crawl = False
+                    break
+                else:
+                    crawl = True
 total_tags = set(total_tags)
 
-old_tags = glob.glob(tag_dir + '*.md')
+old_tags = glob.glob(f'{tag_dir}*.md')
 for tag in old_tags:
     os.remove(tag)
-    
+
 if not os.path.exists(tag_dir):
     os.makedirs(tag_dir)
 
 for tag in total_tags:
     tag_filename = tag_dir + tag + '.md'
-    f = open(tag_filename, 'a')
-    write_str = '---\nlayout: tagpage\ntitle: \"Tag: ' + tag + '\"\ntag: ' + tag + '\nrobots: noindex\n---\n'
-    f.write(write_str)
-    f.close()
+    with open(tag_filename, 'a') as f:
+        write_str = '---\nlayout: tagpage\ntitle: \"Tag: ' + tag + '\"\ntag: ' + tag + '\nrobots: noindex\n---\n'
+        f.write(write_str)
 print("Tags generated, count", total_tags.__len__())
